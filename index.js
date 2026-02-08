2c3756f40df140ff7a3cab372208308a56963ca4
adam-linuxhub <_linux@quirky-mcclintock.90-207-193-202.plesk.page>
2026-02-08 15:40:13 +0000

    Live

--- index.js
+++ index.js
@@ -9,16 +9,11 @@ app.post('/validate-me', (req, res) => {
   const payload = envoy.payload;
   const num_minutes = payload.num_minutes;
   
-  if (isNaN(num_minutes) || num_minutes < 0 || num_minutes > 180) 
-  {
+  if (isNaN(num_minutes) || num_minutes < 0 || num_minutes > 180) {
     res.sendFailed('Minutes must be a number between 0 and 180');
-  }
-  else
-  {
+  } else {
     res.send({ num_minutes, bar: 'Success',});
   }
-
-
 });
 
 // Sign-in
@@ -36,22 +31,19 @@ app.post('/visitor-sign-out', async (req, res) => {
   const envoy = req.envoy; 
   const job = envoy.job;
   const visitor = envoy.payload;
-  const num_minutes = envoy.meta.config.num_minutes;          // Number of minutes allowed from the config
-  const signed_in = visitor.attributes['signed-in-at'];   // Signed-in time
-  const signed_out = visitor.attributes['signed-out-at']; // Signed-out time
+  const num_minutes = envoy.meta.config.num_minutes;        // Number of minutes allowed from the config
+  const signed_in = visitor.attributes['signed-in-at'];     // Signed-in time
+  const signed_out = visitor.attributes['signed-out-at'];   // Signed-out time
   const start = new Date(signed_in);  
   const end = new Date(signed_out);
-  const diffMinutes = Math.floor((end - start) / (1000 * 60)); // Calculating the difference in minutes
+  const diffMinutes = Math.floor((end - start) / (1000 * 60)); // Difference in minutes
 
-  const message = `Visitor stayed within the allotted time  - ${diffMinutes} ${signed_in} ${signed_out} ${num_minutes} ${start} ${end}`;
-  if(diffMinutes > num_minutes)
-  {
+  var message = `Visitor stayed within the allotted time`;
+  if(diffMinutes > num_minutes){
     message = `Visitor stayed over the allotted time  - ${diffMinutes}`;
   } 
 
   await job.attach({ label: 'Visitor Details', value: message });
-  
- // res.send({ num_minutes });
 });
 
 
