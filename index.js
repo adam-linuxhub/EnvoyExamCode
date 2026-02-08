7a54f171f4d04048834f830978a027346dc33fee
adam-linuxhub <_linux@quirky-mcclintock.90-207-193-202.plesk.page>
2026-02-08 15:28:50 +0000

    version 14

--- index.js
+++ index.js
@@ -37,19 +37,19 @@ app.post('/visitor-sign-out', async (req, res) => {
   const job = envoy.job;
   const visitor = envoy.payload;
   const num_minutes = envoy.meta.config.num_minutes;          // Number of minutes allowed from the config
-  const entry_sign_in = visitor.attributes['signed-in-at'];   // Signed-in time
-  const entry_sign_out = visitor.attributes['signed-out-at']; // Signed-out time
-  const start = new Date(entry_sign_in);  
-  const end = new Date(entry_sign_out);
+  const signed_in = visitor.attributes['signed-in-at'];   // Signed-in time
+  const signed_out = visitor.attributes['signed-out-at']; // Signed-out time
+  const start = new Date(signed_in);  
+  const end = new Date(signed_out);
   const diffMinutes = Math.floor((end - start) / (1000 * 60)); // Calculating the difference in minutes
 
-  const message = `Visitor stayed within the allotted time  - ${diffMinutes}`;
+  const message = `Visitor stayed within the allotted time  - ${diffMinutes} {signed_in} {signed_out} {num_minutes} {start} {end}`;
   if(diffMinutes > num_minutes)
   {
     message = `Visitor stayed over the allotted time  - ${diffMinutes}`;
   } 
 
-  await job.attach({ label: 'Goodbye', value: message });
+  await job.attach({ label: 'Visitor Details', value: message });
   
  // res.send({ num_minutes });
 });
